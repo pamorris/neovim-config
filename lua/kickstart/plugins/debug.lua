4ac85f9dba3262c8e6902667cb0e5fa07b4e6d68
Paul Morris <paul@pmorris.org>
2025-01-16 00:15:59 +0000

    Enable dap for python

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -22,7 +22,8 @@ return {
     'jay-babu/mason-nvim-dap.nvim',
 
     -- Add your own debuggers here
-    'leoluz/nvim-dap-go',
+    --'leoluz/nvim-dap-go',
+    'mfussenegger/nvim-dap-python',
   },
   keys = {
     -- Basic debugging keymaps, feel free to change to your liking!
@@ -94,7 +95,8 @@ return {
       -- online, please don't ask me how to install them :)
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
-        'delve',
+        --'delve',
+        'python',
       },
     }
 
@@ -120,6 +122,9 @@ return {
       },
     }
 
+    require('dap-python').setup 'uv'
+    require('dap-python').test_runner = 'pytest'
+
     -- Change breakpoint icons
     -- vim.api.nvim_set_hl(0, 'DapBreak', { fg = '#e51400' })
     -- vim.api.nvim_set_hl(0, 'DapStop', { fg = '#ffcc00' })
@@ -137,12 +142,12 @@ return {
     dap.listeners.before.event_exited['dapui_config'] = dapui.close
 
     -- Install golang specific config
-    require('dap-go').setup {
-      delve = {
-        -- On Windows delve must be run attached or it crashes.
-        -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
-        detached = vim.fn.has 'win32' == 0,
-      },
-    }
+    --require('dap-go').setup {
+    --  delve = {
+    --    -- On Windows delve must be run attached or it crashes.
+    --    -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
+    --    detached = vim.fn.has 'win32' == 0,
+    --  },
+    --}
   end,
 }
