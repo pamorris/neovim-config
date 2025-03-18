512a8d19684c5b22829aeeb7a4f65c1b32916247
Paul Morris <paul@pmorris.org>
2025-03-18 22:57:39 +0000

    Neogit bindings

--- lua/custom/plugins/neogit.lua
+++ lua/custom/plugins/neogit.lua
@@ -5,5 +5,8 @@ return {
     'sindrets/diffview.nvim', -- optional - Diff integration
     'nvim-telescope/telescope.nvim', -- optional
   },
+  keys = {
+    { '<leader>Gs', ':Neogit<CR>', desc = 'Neogit status', silent = true },
+  },
   config = true,
 }
